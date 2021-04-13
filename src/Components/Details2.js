bf10f69d4d6ecf88580a9b48d61c2c3153964ccd
dpattonux <danielnpatton@gmail.com>
2021-04-12 20:20:24 -0400

    Added code for details step 2 page

---
+++ src/Components/Details2.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import "../App.css";
+import logo from "./lifesavrlogo.svg";
+import logorobot from "./lifesavrrobot.svg";
+
+class Details2 extends Component {
+  render() {
+    return (
+      <>
+        <div className="topbar_container">
+          <div className="topbar"></div>
+          <div className="navlogo">
+            <img src={logo} alt="lifesavr logo" />
+          </div>
+        </div>
+        <div className="robot_expenses">
+          <img src={logorobot} alt="robotlogo" />
+        </div>
+        <div className="expenses_text_container">
+          <text className="expenses_text">
+            Jane, tell us about your typical monthly expenses. You can add your
+            banking details or enter manually.
+          </text>
+        </div>
+        <div className="inputcard_container">
+          <div className="inputcard"></div>
+          <button className="connect_bank">Connect Your Bank Account</button>
+          <form>
+            <label className="rent_label">
+              Rent or Mortgage
+              <input type="text" placeholder="$"></input>
+            </label>
+            <label>
+              Credit Card Payments
+              <input type="text" placeholder="$"></input>
+            </label>
+            <label>
+              Car Payment
+              <input type="text" placeholder="$"></input>
+            </label>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Details2;
